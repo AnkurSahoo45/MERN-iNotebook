7858374402ad4b1edc88d70033d05718c537b2e8
AnkurSahoo45 <ankursahoo83213@gmail.com>
2024-05-07 21:53:31 +0530

    done

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -9,7 +9,7 @@ const NoteItem = (props) => {
         <div className='col-md-3'>
             <div className="card my-3">
                 <div className="card-body">
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{zIndex: '1'}}>{note.tag}</span>
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{ zIndex: '1' }}>{note.tag}</span>
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
                     <i className="fa-solid fa-trash mx-2" onClick={() => {
