25b445ba5d21ee43a53a00132a7e01c5c093f654
AnkurSahoo45 <ankursahoo83213@gmail.com>
2024-05-07 21:48:12 +0530

    done

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -9,13 +9,14 @@ const NoteItem = (props) => {
         <div className='col-md-3'>
             <div className="card my-3">
                 <div className="card-body">
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{zIndex: '1'}}>{note.tag}</span>
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
-                    <i className="fa-solid fa-trash mx-2" onClick={()=>{
+                    <i className="fa-solid fa-trash mx-2" onClick={() => {
                         deleteNote(note._id);
                         props.showAlert("Deleted Successfully", "success");
                     }}></i>
-                    <i className="fa-solid fa-pen-to-square mx-2" onClick={()=>{
+                    <i className="fa-solid fa-pen-to-square mx-2" onClick={() => {
                         updateNote(note);
                     }}></i>
                 </div>
